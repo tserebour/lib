a391d5206e310632656d740139a20a486441f4c1
tserebour <tserebour1@gmail.com>
2022-09-05 05:24:03 +0100

    update
    you can clean those commented lines in the main.dart

---
+++ customobjects/customer.dart
@@ -0,0 +1,56 @@
+import 'human.dart';
+import 'package:http/http.dart' as http;
+
+
+class Customer extends Human {
+
+  String lastname = "";
+  String address = "";
+  String email = "";
+  String phone = "";
+
+  Future login() async {
+
+    var url = Uri.http(this.server, '/laundry/login_val.php');
+
+
+    Map user_info = await{
+      'username': this.username,
+      'password': this.password
+
+    };
+
+    var response = await http.post(url, body: user_info);
+
+
+    print('status: ${response.statusCode}');
+    print('status: ${response.body}');
+
+
+
+  }
+
+
+  Future signup() async {
+    var url = Uri.http(this.server, '/laundry/signup.val.php');
+
+
+    Map user_info = await{
+      'username': this.username,
+      'firstname': this.name,
+      'lastname': this.lastname,
+      'address': this.address,
+      'phone': this.phone,
+      'email': this.email,
+      'password': this.password,
+      'confirm_password': this.confirm_password
+    };
+
+    var response = await http.post(url, body: user_info);
+
+
+    print('status: ${response.statusCode}');
+    print('status: ${response.body}');
+  }
+
+}
--- customobjects/human.dart
+++ customobjects/human.dart
@@ -1,3 +1,11 @@
 abstract class Human{
+  final String server = '169.254.248.163';
+  String name = "";
+  String username = "";
+  String password = "";
+  String confirm_password = "";
 
+
+   Future login();
+   Future signup();
 }
